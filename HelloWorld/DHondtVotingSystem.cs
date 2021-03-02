ce267c4228cc8661fe5a1c6fdff9aada153e7b37
Stewart Charles Fisher <25020928@students.lincoln.ac.uk>
2021-03-02 16:48:13 +0000

    Add project files.

---
+++ HelloWorld/DHondtVotingSystem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DHondtVotingCalculator
+{
+    class VoteCount
+    {
+        public static void Main(string[] args)
+        {
+            // Ask how many votes the Brexit Party won
+            
+            double BPVotes = 452321;
+            Console.WriteLine($"The Brexit Party won {BPVotes} votes");
+
+            // Ask how many votes the Liberal Democrats won
+            
+            double LDVotes = 203989;
+            Console.WriteLine($"The Liberal Democrats won {LDVotes} votes");
+
+            // Ask how many votes the Labour Party won
+            
+            double LPVotes = 164682;
+            Console.WriteLine($"The Labour Party won {LPVotes} votes");
+
+            // Ask how many votes the Conservative Party won
+            
+            double CPVotes = 126138;
+            Console.WriteLine($"The Conservative Party won {CPVotes} votes");
+
+            // Ask how many votes the Green Party won
+            
+            double GPVotes = 124630;
+            Console.WriteLine($"The Green Party won {GPVotes} votes");
+
+            // Ask how many votes UKIP won
+            
+            double UKIPVotes = 58198;
+            Console.WriteLine($"UKIP won {UKIPVotes} votes");
+
+            // Ask how many votes Change UK won
+            
+            double CUKVotes = 41117;
+            Console.WriteLine($"Change UK won {CUKVotes} votes");
+
+            // Ask how many votes the Independent Network won
+            
+            double IKVotes = 7641;
+            Console.WriteLine($"The Independent Network won {IKVotes} votes");
+
+            // Ask how many votes the Independents won
+            
+            double IVotes = 4511;
+            Console.WriteLine($"The Independents won {IVotes} votes");
+        }
+    }
+    class VoteCalculation
+    {
+        public static void Main(string[] args)
+        {
+            // Create a array
+        }
+    }
+}
